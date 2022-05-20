2226cb93da6e65d8f5c17c473fc81a4775aa6758
Zihao Li <zihao.li.dev@outlook.com>
2022-05-20 09:06:59 +0000

    examples target as scheduling's benchmark

---
+++ xls/examples/schedule_benchmark_packages.inc.h
@@ -0,0 +1,39 @@
+// Copyright 2020 The XLS Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+#ifndef XLS_SCHEDULE_BENCHMARK_PACKAGES_INC_H_
+#define XLS_SCHEDULE_BENCHMARK_PACKAGES_INC_H_
+
+#include <filesystem>
+#include <string>
+#include <vector>
+
+#include "absl/status/statusor.h"
+#include "xls/common/file/filesystem.h"
+#include "xls/common/file/get_runfile_path.h"
+#include "xls/common/status/status_macros.h"
+
+namespace xls {
+
+inline absl::StatusOr<std::vector<std::string>> GetExamplePaths() {
+  XLS_ASSIGN_OR_RETURN(
+      std::filesystem::path example_file_list_path,
+      GetXlsRunfilePath("xls/examples/scheduling_examples_file_list.txt"));
+  XLS_ASSIGN_OR_RETURN(std::string example_paths_string,
+                       GetFileContents(example_file_list_path));
+  return absl::StrSplit(example_paths_string, '\n');
+}
+
+}  // namespace xls
+
+#endif  // XLS_SCHEDULE_BENCHMARK_PACKAGES_INC_H_
