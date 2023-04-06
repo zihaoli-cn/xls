3efc9d92296853c8f548b58db833bb641921ca80
Zihao Li <li0037@hotmail.com>
2023-04-06 21:48:06 +0800

    update benchmark

--- xls/p5/data/benchmark/rename.sh
+++ xls/p5/data/benchmark/rename.sh
@@ -6,5 +6,10 @@ declare -a json_files=($(find . -name "*.json"))
 # 遍历所有的json文件，使用 diff 命令比较每一对文件
 for ((i=0; i<${#json_files[@]}; i++))
 do
-    mv "${json_files[i]}" "bench$i.json"
+    if [ -f "bench$i.json" ]; then
+        echo "file bench$i.json exists"
+    else
+        mv "${json_files[i]}" "bench$i.json"
+    fi
+    
 done
